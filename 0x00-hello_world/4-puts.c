7607cf7df6b38dbcd80d82b3f4b5dde44ee01be9
VMP1312 <victor.paz1312@hotmail.com>
2020-02-05 18:39:05 +0000

    Fix file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+ * main - Print a string.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char string[80] = "\"Programming is like building a multilingual puzzle";
+
 	puts(string);
 	return (0);
 }
