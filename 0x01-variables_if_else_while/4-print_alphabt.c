f6aee5578e6cc68a5ca043d28f3063debc6cd329
VMP1312 <victor.paz1312@hotmail.com>
2020-02-06 19:07:09 +0000

    Fix issues

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,11 @@ int main(void)
 
 	for (c = 'a'; c <= 'z'; ++c)
 	{
-		if (c != 'e' && c != 'q'){
+		if (c != 'q' && c != 'e')
+		{
 			putchar(c);
 		}
-		putchar('\n');
-		return (0);
 	}
+	putchar('\n');
+	return (0);
+}
