22029f6aff79c9acf58a0bfd52b44c24387eae90
VMP1312 <victor.paz1312@hotmail.com>
2020-02-11 18:50:56 +0000

    fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 /**
  * _islower - returns 1 if lowercase alphabet and 0 if not
+ * @c: character to print.
  * Return: 0 for uppercase, 1 for lowercase
  */
 int _islower(int c)
