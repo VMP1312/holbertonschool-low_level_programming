efd9304ba6c20483ec1a8ae659778896f11db409
VMP1312 <victor.paz1312@hotmail.com>
2020-02-13 15:53:22 +0000

    Create

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplicate two integers
+ * @a: number 1
+ * @b: number 2
+ * Return: multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
