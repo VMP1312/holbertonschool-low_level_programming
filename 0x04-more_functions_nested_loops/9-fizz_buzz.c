16f78cb7bf9c6cb8e692f4a299a59e8d917e5f17
VMP1312 <victor.paz1312@hotmail.com>
2020-02-13 20:33:26 -0500

    Fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,11 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 3 == 0)
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
 		{
 			printf("Fizz");
 		}
@@ -20,15 +24,11 @@ int main(void)
 		{
 			printf("Buzz");
 		}
-		else if (n % 3 == 0 && n % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
 		else
 		{
 			printf("%d", n);
 		}
-		if (n < 100)
+		if (num < 100)
 		{
 			printf(" ");
 		}
