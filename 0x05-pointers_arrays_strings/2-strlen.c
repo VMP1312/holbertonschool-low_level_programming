3ccaabfce51e5df8370696f6ab67ef5085a4383c
VMP1312 <victor.paz1312@hotmail.com>
2020-02-17 17:30:08 +0000

    Create

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strlen - return the length of a string
+ * @s: pointer of the array
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] !='\0'; c++)
+	{
+	}
+	return (c);
+}
