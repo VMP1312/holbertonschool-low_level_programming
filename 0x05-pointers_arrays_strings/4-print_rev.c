a3acba271cd8cd188d5f79702f10ce142bc54139
VMP1312 <victor.paz1312@hotmail.com>
2020-02-18 14:29:41 -0500

    Fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * print_rev - prints a string, in reverse, followed by a new line
- * @s: string to reverse
+ * print_rev - prints a string backwards
+ * @s: string
  */
 
 void print_rev(char *s)
@@ -12,13 +12,13 @@ void print_rev(char *s)
 
 	while (*s != '\0')
 	{
-		i++;
+		len++;
 		++s;
 	}
 
 	s--;
 
-	while ( i > 0)
+	while (i > 0)
 	{
 		_putchar(*s);
 		s--;
