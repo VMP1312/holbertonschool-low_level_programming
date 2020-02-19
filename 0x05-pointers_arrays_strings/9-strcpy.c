313613f8e82244da9555155bc9c1085d83b97756
VMP1312 <victor.paz1312@hotmail.com>
2020-02-18 23:02:32 -0500

    Fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * *_strcpy - Copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest.
+ * _strcpy - Copies the string pointed to by src, including the null byte
  * @dest: char dest
  * @src: char src
  * Return: dest.
