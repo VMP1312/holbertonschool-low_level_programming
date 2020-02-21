db6989cb6d557ec32cc1e7a2de0e39eafc80958a
VMP1312 <victor.paz1312@hotmail.com>
2020-02-20 19:18:49 -0500

    Create

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * string_toupper -changes lowecase into uppercase
+ * @a: take an string
+ * Return: the string
+ */
+char *string_toupper(char *a)
+{
+	int b;
+
+	for (b = 0; a[b] != '\0'; b++)
+	{
+		if (a[b] >= 'a' && a[b] <= 'z')
+		{
+			a[b] = a[b] - 32;
+		}
+	}
+	return (a);
+}
