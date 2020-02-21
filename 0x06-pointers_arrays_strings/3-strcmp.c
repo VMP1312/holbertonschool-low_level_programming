11cfcd70690459c2dc483ee4a4cca975f90e8046
VMP1312 <victor.paz1312@hotmail.com>
2020-02-20 19:00:30 -0500

    Create

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: q = to the s1.
+ * @s2: p = to the s2.
+ * Return: s1, s2 or 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
