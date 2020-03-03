ff5cb3e7d5a219623c92cf338bc7ff20306279d0
VMP1312 <victor.paz1312@hotmail.com>
2020-03-03 18:55:37 -0500

    Fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,11 +23,11 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	for (i = 0; s1[i] != '\0')
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		;
 	}
-	for (j = 0; s2[j] != '\0')
+	for (j = 0; s2[j] != '\0'; j++)
 	{
 		;
 	}
