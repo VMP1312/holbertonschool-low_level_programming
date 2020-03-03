1d82eea6a0348c46ffd64866f6e7ee548e106225
VMP1312 <victor.paz1312@hotmail.com>
2020-03-03 18:50:25 -0500

    Fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,12 +9,12 @@
  **/
 char *str_concat(char *s1, char *s2)
 {
-	int j;
 	int i;
+	int j;
 	int k;
 	int a;
 	char *s;
-
+	
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -23,13 +23,15 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	for (i = 0; s1[i] != '\0'; i++)
+	i = 0;
+	while (s1[i] != '\0')
 	{
-		;
+		i++;
 	}
-	for (j = 0; s1[j] != '\0'; j++)
+	j = 0;
+	while (s2[j] != '\0')
 	{
-		;
+		j++;
 	}
 	s = malloc(sizeof(char) * (i + j + 1));
 	if (s == NULL)
