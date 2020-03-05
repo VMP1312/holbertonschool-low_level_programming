e94ea2d29ef4bede6c2b8992690eb18cad9c5062
VMP1312 <victor.paz1312@hotmail.com>
2020-03-05 17:16:01 +0000

    Create

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,9 +1,52 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ *string_nconcat - concat 2 strings
+ *@s1: string
+ *@s2: string
+ *@n: pos of the string
+ *Return: the concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, k, m;
+	char *c;
 
-#include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	if (n >= j)
+	{
+	n = j;
+	}
+	c = malloc(sizeof(*c) * (i + n + 1));
+		if (c == NULL)
+		{
+			return (NULL);
+		}
+	for (k = 0; k < i; k++)
+	{
+		c[k] = s1[k];
+	}
+	for (m = 0; m < n; m++)
+	{
+		c[i + m] = s2[m];
+	}
+	c[i + m] = '\0';
+	return (c);
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,7 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 int _putchar(char c);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif
