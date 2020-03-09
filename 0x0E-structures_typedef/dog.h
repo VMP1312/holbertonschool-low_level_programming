98cf53a4125d0d2e36b633eef778c10ed062b791
VMP1312 <victor.paz1312@hotmail.com>
2020-03-09 20:15:21 +0000

    Create

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG
+#define DOG
+/**
+ *struct dog -Structure of dog
+ *@name: name 
+ *@age: age
+ *@owner: name of the owner
+ */
+struct dog 
+{
+	char *name;
+	char *owner;
+	float age;
+};
