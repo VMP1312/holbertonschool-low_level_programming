641fbf75ce4de88a70b24d6371d3976856e5d02d
VMP1312 <victor.paz1312@hotmail.com>
2020-03-10 01:13:30 +0000

    Create

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *new_dog - Creates a new dog.
+ *@name: Name
+ *@age: Age
+ *@owner: Owner
+ *Return: 0
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	int i, j, k, a;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; owner[j] != '\0'; j++)
+	{
+	}
+	d->name = malloc(sizeof(char) * i + 1);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	for (k = 0; k < (i + 1); k++)
+		d->name[k] = name[k];
+	d->age = age;
+	d->owner = malloc(sizeof(char) * (j + 1));
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	for (a = 0; a < (j + 1); a++)
+		d->owner[a] = owner[a];
+	return (d);
+}
