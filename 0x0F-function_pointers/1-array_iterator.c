e7008b2cfce85ed257c047ab7b8285701871de30
VMP1312 <victor.paz1312@hotmail.com>
2020-03-11 19:27:51 +0000

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,11 +1,22 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator -
+ * array_iterator - 
  *
  * Return: 
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-
+	int i;
+	int j;
+	
+	j = size;
+	if (size <= 0 || array == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < j; i++)
+	{
+		action(array[i]);
+	}
 }
