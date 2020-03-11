2ab7fb598475c8cabec1273da2e60765b67b017b
VMP1312 <victor.paz1312@hotmail.com>
2020-03-11 22:08:38 +0000

    Create

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,11 +1,24 @@
 #include "function_pointers.h"
 
 /**
- * int_index -
- *
+ * int_index - Searches for an integer.
+ *@array: Array
+ *@size: Size of array
+ *@cmp: pointer to function
  * Return: 
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
 
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		return (i);
+	}
+	return (1);
 }
