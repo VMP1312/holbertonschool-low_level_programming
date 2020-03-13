c581d4ad5248f15ea67334e5616ef1fb2aa9b4f4
VMP1312 <victor.paz1312@hotmail.com>
2020-03-13 18:05:15 +0000

    Create

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all -
+ *
+ * Return: 
+ */
+void print_all(const char * const format, ...)
+{
+	va_list l;
+	unsigned int i = 0, j = 1;
+	char *x;
+
+	while (format)
+	{
+		va_start(l, format);
+		while (format[i])
+		{
+			j = 1;
+			switch (format[i])
+			{
+			case 'c':
+				printf("%c", va_arg(l, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(l, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(l, double));
+				break;
+			case 's':
+				x = va_arg(l, char *);
+				if (x == NULL)
+					x = "(nil)";
+				printf("%s", x);
+				break;
+			default:
+				j = 0;
+				break;
+			}
+			if (format[i+1] && j)
+				printf(", ");
+			i++;
+		}
+		va_end(l);
+		break;
+	}
+	printf("\n");
+}
