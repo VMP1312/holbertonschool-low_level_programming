5a8bf84303e6aaa0dd020204d28759ea84fb880b
VMP1312 <victor.paz1312@hotmail.com>
2020-03-19 23:18:59 +0000

    Fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,7 +4,7 @@
  * add_node - adds a new node at the beginning of a list.
  * @head: pointer
  * @str: pointer
- * Return: address of the new element. 
+ * Return: address of the new element.
  */
 list_t *add_node(list_t **head, const char *str)
 {
