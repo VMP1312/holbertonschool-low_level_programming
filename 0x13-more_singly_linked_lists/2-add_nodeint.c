30e1cc10dcaf38856a5a5e64f2d490d7f1f892a7
VMP1312 <victor.paz1312@hotmail.com>
2020-03-23 19:02:05 +0000

    Fix

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,13 +3,13 @@
 /**
  * add_nodeint - Adds a new node at the beginning of a list.
  * @head: Node.
- * @n: Number. 
+ * @n: Number.
  * Return: Node.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *node;
-	
+
 	node = malloc(sizeof(listint_t));
 
 	if (node == NULL)
