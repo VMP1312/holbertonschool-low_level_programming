1f924cc45943546c5fcfd45091360f6c0f9b4774
VMP1312 <victor.paz1312@hotmail.com>
2020-03-23 17:53:40 +0000

    Fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * print_listint -
- *
- * Return: 
+ * print_listint - prints all the elements of a list
+ *@h: node
+ * Return: Number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
