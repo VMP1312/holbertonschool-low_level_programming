bbb2aa3340abf8ec8a857646f4a391baa1bb4308
VMP1312 <victor.paz1312@hotmail.com>
2020-03-24 03:00:47 +0000

    Create

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Function that returns the sum of all the data (n) of a list
+ * @head: Node.
+ * Return: Sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	i = 0;
+
+	if (!head)
+		return (sum);
+
+	if (head->next)
+		sum += sum_listint(head->next);
+
+	sum += head->n;
+	return (sum);
+}
