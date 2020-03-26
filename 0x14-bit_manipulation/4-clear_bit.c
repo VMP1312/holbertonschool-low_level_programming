dc06a744ef565822c7b30be177bc9ff0f3a0c98d
VMP1312 <victor.paz1312@hotmail.com>
2020-03-26 21:17:26 +0000

    Fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- * clear_bit -
- *
- * Return: 
+ * clear_bit - Sets the value of a bit to 0 at a given index.
+ * @index: Index.
+ * @n: Number.
+ * Return: 1 or -1 on error.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
