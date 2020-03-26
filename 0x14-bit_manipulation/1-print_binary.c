87b0b2c740a01686508d2de9eba44b5e1dc8554e
VMP1312 <victor.paz1312@hotmail.com>
2020-03-26 17:27:37 +0000

    Create

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+ * print_binary -
+ *
+ * Return: 
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int bin = 1;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+
+	bin = bin << 63;
+	while (!(n & bin))
+	{
+		bin = bin >> 1;
+	}
+
+	while (bin)
+	{
+		if ((n & bin) != 0)
+		{
+			_putchar('1');
+			bin = bin >> 1;
+		}
+		else
+		{
+			_putchar('0');
+			bin = bin >> 1;
+		}
+	}
+}
