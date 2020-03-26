90a65f41d1983b5757be8030a67ff89c520c4ce3
VMP1312 <victor.paz1312@hotmail.com>
2020-03-26 17:28:56 +0000

    Fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
 
 /**
- * print_binary -
- *
- * Return: 
+ * print_binary - Prints the binary representation of a number.
+ * @n: Number.
  */
 void print_binary(unsigned long int n)
 {
