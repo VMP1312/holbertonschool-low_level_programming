c2562f0872e16f395475ed37a86284109196b182
VMP1312 <victor.paz1312@hotmail.com>
2020-03-31 19:43:34 +0000

    Fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,7 +3,7 @@
 /**
  * read_textfile - reads a text file & prints it to the POSIX standard output.
  * @filename: Filename.
- * @Letters: # of letters to print.
+ * @letters: # of letters to print.
  * Return: The actual number of letters it could read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -17,16 +17,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL || buf == NULL)
 		return (0);
 
-
-
 	/*OPEN*/
-
 	op = open(filename, O_RDONLY);
 	if (op == -1)
 		return (0);
 
 	/*READ*/
-
 	rd = read(op, buf, letters);
 	if (rd == -1)
 	{
@@ -36,7 +32,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	/*WRITE*/
-
 	wr = write(STDOUT_FILENO, buf, rd);
 	if (wr == -1)
 	{
