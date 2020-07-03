36fb75bd49aa997b71406c8842980ebd2e9a60e3
VMP1312 <victor.paz1312@hotmail.com>
2020-07-03 20:49:29 +0000

    Create

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Island perimeter"""
+
+
+def island_perimeter(grid):
+    """Function that return the mesure perimeter island"""
+    perim = 0
+    for x in range(len(grid)):
+        for y in range(len(grid)):
+            if grid[x][y] == 1:
+                perim += 4
+                if grid[x + 1][y] == 1:
+                    perim -= 1
+                if x > 0 and grid[x - 1][y] == 1:
+                    perim -= 1
+                if grid[x][y + 1] == 1:
+                    perim -= 1
+                if y > 0 and grid[x][y - 1] == 1:
+                    perim -= 1
+    return perim
