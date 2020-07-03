8bf7cadce7be5c5706e991cbbfd59b7296d49aae
VMP1312 <victor.paz1312@hotmail.com>
2020-07-03 21:08:13 +0000

    Fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,7 +6,7 @@ def island_perimeter(grid):
     """Function that return the mesure perimeter island"""
     perim = 0
     for x in range(len(grid)):
-        for y in range(len(grid)):
+        for y in range(len(grid[0])):
             if grid[x][y] == 1:
                 perim += 4
                 if grid[x + 1][y] == 1:
