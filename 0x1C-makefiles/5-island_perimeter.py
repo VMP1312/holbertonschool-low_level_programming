5a10bbf24abd5d648aa85cb223990c5a3ef66bbd
VMP1312 <victor.paz1312@hotmail.com>
2020-07-03 20:52:05 +0000

    Fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,11 +9,11 @@ def island_perimeter(grid):
         for y in range(len(grid)):
             if grid[x][y] == 1:
                 perim += 4
-                if grid[x + 1][y] == 1:
+                if x > 0 and grid[x + 1][y] == 1:
                     perim -= 1
                 if x > 0 and grid[x - 1][y] == 1:
                     perim -= 1
-                if grid[x][y + 1] == 1:
+                if x > 0 and grid[x][y + 1] == 1:
                     perim -= 1
                 if y > 0 and grid[x][y - 1] == 1:
                     perim -= 1
